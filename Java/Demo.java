c5b731040d03c78f109fe71bb5bbc2cf41d0966f
Max <trex.eternatus@gmail.com>
2025-12-15 19:37:30 +0530

    Add a few files

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,10 +1,24 @@
-import java.util.Scanner;
-
 public class Demo{
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String name = scn.next();
-        System.out.println("Hi "  + name);
-        scn.close();
+    public boolean containsDuplicate(int[] nums) {
+        boolean res = true;
+        for (int i = 0;i!=(nums.length)-1;i++)
+        {
+            for (int j=i+1; j<(nums.length)-1; j++)
+            {
+                if (nums[i]>nums[j])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+            for (int k=i+1; k<(nums.length)-1; k++)
+            {
+                if(nums[i]==nums[k])
+                res = false;
+                break;
+            }
+        }
+        return res;
     }
 }
