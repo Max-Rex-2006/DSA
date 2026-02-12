b7e331d6ec47eb66f60bf5f92428f94fa705d38e
Max Rex <trex.eternatus@gmail.com>
2026-02-12 21:05:03 +0530

    Add Day12

---
+++ Java/hundredDaysOfCode/src/Day12.java
@@ -0,0 +1,10 @@
+public class Day12 {
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+    public static void main(String[] args) { 
+        Day12 d = new Day12(); 
+        System.out.println(d.isStrictlyPalindromic(9)); 
+    }
+    
+}
