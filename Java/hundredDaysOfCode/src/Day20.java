2557168769e7687c3f9652f57d2f653b7bff7682
Max Rex <trex.eternatus@gmail.com>
2026-02-20 22:17:58 +0530

    Add Day20

--- Java/hundredDaysOfCode/src/Day20.java
+++ Java/hundredDaysOfCode/src/Day20.java
@@ -1,3 +1,21 @@
 public class Day20 {
-
+    public int maxScore(String s) {
+        int max = 0;
+        for(int i=1; i<s.length(); i++){
+            int val = 0;
+            for(int j=0; j<i; j++){
+                if(s.charAt(j)=='0') val++;
+            }
+            for(int j=i; j<s.length(); j++){
+                if(s.charAt(j)=='1') val++;
+            }
+            if(val>max) max = val;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Day20 day20 = new Day20();
+        String s = "011101";
+        System.out.println(day20.maxScore(s));
+    }
 }
