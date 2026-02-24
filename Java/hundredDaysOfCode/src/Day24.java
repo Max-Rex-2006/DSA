c7ad19a513254e335a26b51712b4a0d2dbccad9a
Max Rex <trex.eternatus@gmail.com>
2026-02-24 22:09:20 +0530

    Add Day24

---
+++ Java/hundredDaysOfCode/src/Day24.java
@@ -0,0 +1,25 @@
+public class Day24 {
+    public int searchInsert(int[] nums, int target) {
+        int left=0, right=nums.length-1;
+        int mid = left+(right-left)/2;
+        while(left<=right){
+            mid = left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+    public static void main(String[] args) {
+        Day24 day24 = new Day24();
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        System.out.println(day24.searchInsert(nums, target));
+    }
+}
