2f24d1e4e670e274b1e0253ef4f26e7c03d77e46
Max Rex <trex.eternatus@gmail.com>
2026-03-02 23:15:24 +0530

    Add Day27

--- Java/hundredDaysOfCode/src/Day27.java
+++ Java/hundredDaysOfCode/src/Day27.java
@@ -1,3 +1,17 @@
 public class Day27 {
-
+    public boolean isPowerOfFour(int n) {
+        while(n>9){
+            if(n%4!=0)
+                return false;
+            n/=4;
+        }
+        if(n==1||n==4)
+            return true;
+        return false;
+    }
+    public static void main(String[] args) {
+        Day27 d = new Day27();
+        int n = 16;
+        System.out.println(d.isPowerOfFour(n));
+    }
 }
