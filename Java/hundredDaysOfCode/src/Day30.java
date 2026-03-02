4e67ed6ce870802a811367690f89f5a45614ea05
Max Rex <trex.eternatus@gmail.com>
2026-03-02 23:19:44 +0530

    Add Day30

--- Java/hundredDaysOfCode/src/Day30.java
+++ Java/hundredDaysOfCode/src/Day30.java
@@ -1,3 +1,26 @@
-public class Day30 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Day30 {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i%3==0&&i%5==0)
+                ans.add("FizzBuzz");
+            else if(i%3==0)
+                ans.add("Fizz");
+            else if(i%5==0)
+                ans.add("Buzz");
+            else
+                ans.add(Integer.toString(i));
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Day30 d = new Day30();
+        int n = 15;
+        List<String> ans = d.fizzBuzz(n);
+        for(String i:ans)
+            System.out.print(i+" ");
+    }
 }
