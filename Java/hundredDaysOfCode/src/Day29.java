e3c0e63404877492b7a52ad92af8067d60d83d62
Max Rex <trex.eternatus@gmail.com>
2026-03-02 23:18:47 +0530

    Add Day29

--- Java/hundredDaysOfCode/src/Day29.java
+++ Java/hundredDaysOfCode/src/Day29.java
@@ -1,3 +1,19 @@
 public class Day29 {
-
+    public String intToRoman(int num) {
+        String[] romanCode = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] decVal = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        StringBuilder roman = new StringBuilder();
+        for(int i=0; i<romanCode.length; i++){
+            while(num >= decVal[i]){
+                num -= decVal[i];
+                roman.append(romanCode[i]);
+            }
+        }
+        return roman.toString();
+    }
+    public static void main(String[] args) {
+        Day29 d = new Day29();
+        int num = 1994;
+        System.out.println(d.intToRoman(num));
+    }
 }
