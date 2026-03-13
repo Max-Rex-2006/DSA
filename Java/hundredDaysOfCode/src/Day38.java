888bfbc0a06a8e1f0bb525554ef4f168befdd6c7
Max Rex <trex.eternatus@gmail.com>
2026-03-13 22:32:08 +0530

    Day37

--- Java/hundredDaysOfCode/src/Day38.java
+++ Java/hundredDaysOfCode/src/Day38.java
@@ -1,3 +1,21 @@
 public class Day38 {
-
+    public int addRungs(int[] rungs, int dist) {
+        int count=0;
+        if(rungs[0] > dist){
+            count += (rungs[0]-1)/dist;
+        }
+        for(int i=1; i<rungs.length; i++){
+            if((rungs[i]-rungs[i-1])>dist){
+                count += (rungs[i]-rungs[i-1]-1)/dist;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Day38 day38 = new Day38();
+        int[] rungs = {1, 3, 5, 10};
+        int dist = 2;
+        int result = day38.addRungs(rungs, dist);
+        System.out.println(result);
+    }
 }
